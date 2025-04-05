b66ecc862ce72772bf2a1082863244401122029c
lanjua5899 <juanma_boca13@hotmail.com>
2025-04-05 04:13:02 +0200

    commit inicial

---
+++ pythonbasic/00_helloworld.py
@@ -0,0 +1,24 @@
+# Hola Mundo
+print("Hello World!")
+print('Hola Mundo!')
+# Este es un simple script de Python que imprime un mensaje en la consola.
+
+# Este es un comentario de una sola línea
+
+"""
+Este es un comentario de varias líneas.
+Podés escribir varias líneas de texto acá.
+"""
+
+'''
+Este también es un comentario de varias líneas.
+Podés escribir varias líneas de texto acá también.
+'''
+
+# Consultar el tipo de dato
+print(type("Soy un dato str"))  # Tipo 'str'
+print(type(5))  # Tipo 'int'
+print(type(1.5))  # Tipo 'float'
+print(type(True))  # Tipo 'bool'
+print(type(1 + 3j))  # Tipo 'complex'
+print(type(print("Mi cadena de texto")))  # Tipo 'NoneType'
