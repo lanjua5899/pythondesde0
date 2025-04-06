e5668f67d0fa5cb6412cdca77f58baf071e6ba0f
lanjua5899 <juanma_boca13@hotmail.com>
2025-04-06 22:55:48 +0200

    creando tuplas

---
+++ pythonbasic/05_tuples.py
@@ -0,0 +1,53 @@
+### Tuplas ###
+
+# Definición
+
+my_tuple = tuple()
+my_other_tuple = ()
+
+my_tuple = (33, 1.73, "Juan", "Perez Lang", "Juan")
+my_other_tuple = (35, 60, 30)
+
+print(my_tuple)
+print(type(my_tuple))
+
+# Acceso a elementos y búsqueda
+
+print(my_tuple[0])
+print(my_tuple[-1])
+# print(my_tuple[4])  IndexError
+# print(my_tuple[-6])  IndexError
+
+print(my_tuple.count("Juan"))
+print(my_tuple.index("Perez Lang"))
+print(my_tuple.index("Juan"))
+
+# my_tuple[1] = 1.80  las tuplas son inmutables
+# print(my_tuple)
+
+# Concatenación
+
+my_sum_tuple = my_tuple + my_other_tuple
+print(my_sum_tuple)
+
+# Subtuplas
+
+print(my_sum_tuple[3:6])
+
+# Tupla mutable con conversión a lista
+
+my_tuple = list(my_tuple)
+print(type(my_tuple))
+
+my_tuple[4] = "Lanjua"
+my_tuple.insert(1, "Azul")
+my_tuple = tuple(my_tuple)
+print(my_tuple)
+print(type(my_tuple))
+
+# Eliminación
+
+# del my_tuple[2] TypeError: 'tuple' object doesn't support item deletion
+
+del my_tuple
+# print(my_tuple) NameError: name 'my_tuple' is not defined
