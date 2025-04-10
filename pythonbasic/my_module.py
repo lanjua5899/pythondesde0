d1361c46228115f619aca3e5094e77375d6afe15
lanjua5899 <juanma_boca13@hotmail.com>
2025-04-10 04:37:06 +0200

    creando e importando modulos

---
+++ pythonbasic/my_module.py
@@ -0,0 +1,8 @@
+### Módulo para pruebas de importación ###
+
+def sumValue(numberOne, numberTwo, numberThree):
+    print(numberOne + numberTwo + numberThree)
+
+
+def printValue(value):
+    print(value)
