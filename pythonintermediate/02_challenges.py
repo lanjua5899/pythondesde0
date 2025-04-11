42fed194c3663804e553d80e23c24b62ce04cffb
lanjua5899 <juanma_boca13@hotmail.com>
2025-04-11 17:28:42 +0200

    tercer, cuarto y quinto challenge

--- pythonintermediate/02_challenges.py
+++ pythonintermediate/02_challenges.py
@@ -25,13 +25,13 @@ def fizz_buzz():
 fizz_buzz()
 
 """
-¿ES UN ANAGRAMA?
+¿ES UN ANAGRAMA?:
 Escribe una función que reciba dos palabras (String) y retorne
 verdadero o falso (Bool) según sean o no anagramas.
-- Un Anagrama consiste en formar una palabra reordenando TODAS 
-    las letras de otra palabra inicial.
-- NO hace falta comprobar que ambas palabras existan.
-- Dos palabras exactamente iguales no son anagrama.
+ - Un Anagrama consiste en formar una palabra reordenando TODAS
+ las letras de otra palabra inicial.
+ - NO hace falta comprobar que ambas palabras existan.
+ - Dos palabras exactamente iguales no son anagrama.
 """
 
 
@@ -42,3 +42,72 @@ def is_anagram(word_one, word_two):
 
 
 print(is_anagram("Amor", "Roma"))
+
+"""
+LA SUCESIÓN DE FIBONACCI
+Escribe un programa que imprima los 50 primeros números de la sucesión
+de Fibonacci empezando en 0.
+- La serie Fibonacci se compone por una sucesión de números en
+  la que el siguiente siempre es la suma de los dos anteriores.
+  0, 1, 1, 2, 3, 5, 8, 13...
+"""
+
+
+def fibonacci():
+
+    prev = 0
+    next = 1
+
+    for i in range(0, 50):
+        print(prev)
+        fib = prev + next
+        prev = next
+        next = fib
+
+
+fibonacci()
+
+
+"""
+¿ES UN NÚMERO PRIMO?
+Escribe un programa que se encargue de comprobar si un número es o no primo.
+Hecho esto, imprime los números primos entre 1 y 100.
+"""
+
+
+def is_prime():
+
+    for number in range(1, 101):
+
+        if number >= 2:
+
+            is_divisible = False
+
+            for i in range(2, number):
+                if number % i == 0:
+                    is_divisible = True
+                    break
+            if not is_divisible:
+                print(number)
+
+
+is_prime()
+
+
+"""
+INVIRTIENDO CADENAS
+Crea un programa que invierta el orden de una cadena de texto
+sin usar funciones propias del lenguaje que lo hagan de forma automática.
+- Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+"""
+
+
+def reverse(text):
+    text_len = len(text) - 1
+    reversed_text = ""
+    for i in range(0, len(text)):
+        reversed_text += text[text_len - i]
+    return reversed_text
+
+
+print(reverse("Hola mundo"))
