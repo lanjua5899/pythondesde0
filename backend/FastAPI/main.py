78c4841292911c28331f0356a47d8a246e9db82f
lanjua5899 <juanma_boca13@hotmail.com>
2025-04-16 14:29:27 +0200

    implementando mi primera FastAPI

--- backend/FastAPI/main.py
+++ backend/FastAPI/main.py
@@ -1,10 +1,40 @@
 ### FastAPI app ###
 
+# FastAPI es un framework para construir APIs con Python 3.6+ basado en tipos de datos estándar de Python.
+# FastAPI es rápido (alto rendimiento), fácil de usar, y tiene una gran documentación.
+# FastAPI es ideal para construir APIs RESTful y microservicios.
+
+# Instalación de FastAPI: pip install "fastapi[standard]"
+
+from typing import Union
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
+# Url local: http://127.0.0.1:8000
+
 
 @app.get("/")
-async def root():
-    return {"message": "Hola FastAPI!"}
+async def read_root():
+    return {"mensaje": "Hola FastAPI!"}
+
+# Url local: http://127.0.0.1:8000/items/5?q=somequery
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+# Url local: http://127.0.0.1:8000/url
+
+
+@app.get("/url")
+async def read_url():
+    return {"url": "http://linktree.com/juanmathiasperezlang"}
+
+# Iniciar el servidor: fastapi dev main.py --reload
+# Detener el servidor: Ctrl + C
+
+# Documentación con Swagger: http://127.0.0.1:8000/docs
+# Documentación con Redocly: http://127.0.0.1:8000/redoc
