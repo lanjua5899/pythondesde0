83d287c89a3a6a2297b5b3953c7690f5c06ab7be
lanjua5899 <juanma_boca13@hotmail.com>
2025-04-19 15:23:20 +0200

    agregando directorios static e images y tambien imagen estatica

--- backend/FastAPI/main.py
+++ backend/FastAPI/main.py
@@ -9,10 +9,12 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from routers import users, products
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 # Routers
 app.include_router(users.router)  # Incluye el router de usuarios
 app.include_router(products.router)  # Incluye el router de productos
