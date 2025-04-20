7644203dea30b83ef94ca1e8929dcd3246881007
lanjua5899 <juanma_boca13@hotmail.com>
2025-04-20 20:21:59 +0200

    añadiendo un ejemplo de bases de datos

--- backend/FastAPI/routers/basic_auth_users.py
+++ backend/FastAPI/routers/basic_auth_users.py
@@ -1,6 +1,31 @@
 ### Users API con autenticación OAuth2 básica ###
 
-from fastapi import FastAPI
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+router = APIRouter()
 
 
-app = FastAPI()
+class User(BaseModel):
+    username: str
+    full_name: str
+    email: str
+    disabled: bool
+
+
+users_db = {
+    "lanjua": {
+        "username": "lanjua",
+        "full_name": "Juan Perez Lang",
+        "email": "lanjua@lanjua.com",
+        "disabled": False,
+        "password": "123456"
+    },
+    "lanjua2": {
+        "username": "lanjua2",
+        "full_name": "Juan Perez Lang 2",
+        "email": "lanjua2@lanjua.com",
+        "disabled": True,
+        "password": "654321",
+    }
+}
