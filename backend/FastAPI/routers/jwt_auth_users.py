f97949c6d48a67fe30dcff6f922f9e55b68d0c36
lanjua5899 <juanma_boca13@hotmail.com>
2025-04-25 16:38:15 +0200

    validación de token, recuperación de usuario y verificación de estado

--- backend/FastAPI/routers/jwt_auth_users.py
+++ backend/FastAPI/routers/jwt_auth_users.py
@@ -53,6 +53,35 @@ def search_user_db(username: str):
         return UserDB(**users_db[username])
 
 
+def search_user(username: str):
+    if username in users_db:
+        return User(**users_db[username])
+
+
+async def auth_user(token: str = Depends(oauth2)):
+
+    exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                              detail="Credenciales de autenticación inválidas", headers={"WWW-Authenticate": "Bearer"})
+
+    try:
+        username = jwt.decode(
+            token, SECRET_KEY, algorithms=ALGORITHM).get("sub")
+        if username is None:
+            raise exception
+
+    except JWTError:
+        raise exception
+
+    return search_user(username)
+
+
+async def current_user(user: User = Depends(auth_user)):
+    if user.disabled:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Usuario inactivo")
+    return user
+
+
 @router.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
     """
@@ -73,3 +102,8 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
     ) + timedelta(minutes=ACCESS_TOKEN_DURATION_MINUTES)}
 
     return {"access_token": jwt.encode(access_token, SECRET_KEY, algorithm=ALGORITHM), "token_type": "bearer"}
+
+
+@router.get("/users/me")
+async def me(user: User = Depends(current_user)):
+    return user
