cf637a95cd418ded26fd5b40b70aa24e35481696
lanjua5899 <juanma_boca13@hotmail.com>
2025-04-29 20:51:17 +0200

    creando schema usuario y añadiendo get y post a usuarios de base de datos

--- backend/FastAPI/routers/users_db.py
+++ backend/FastAPI/routers/users_db.py
@@ -1,6 +1,41 @@
 ### Users DB API ###
 
 from fastapi import APIRouter, HTTPException, status
+from db.models.user import User
+from db.client import db_client
+from db.schemas.user import users_schema, user_schema
+from bson import ObjectId
+
 
 router = APIRouter(prefix="/usersdb", tags=["usersdb"], responses={
                    status.HTTP_404_NOT_FOUND: {"descripción": "No encontrado"}})
+
+
+@router.get("/", response_model=list[User])
+async def users():
+    return users_schema(db_client.users.find())
+
+
+@router.get("/{id}")
+async def user(id: str):
+    return search_user("_id", ObjectId(id))
+
+
+@router.get("/")
+async def user(id: str):
+    return search_user("_id", ObjectId(id))
+
+
+@router.post("/", response_model=User, status_code=status.HTTP_201_CREATED)
+async def user(user: User):
+    if type(search_user("email", user.email)) == User:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="El usuario ya existe")
+    user_dict = dict(user)
+    del user_dict["id"]
+
+    id = db_client.users.insert_one(user_dict).inserted_id
+
+    new_user = user_schema(db_client.users.find_one({"_id": id}))
+
+    return User(**new_user)
